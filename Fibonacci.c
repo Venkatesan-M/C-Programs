7fdf2a30dfb0864265b4234fedb8ef9c088216fa
Venkatesan M <127939893+Venkatesan-M@users.noreply.github.com>
2023-03-16 22:05:23 +0530

    Update Fibonacci.c

--- Fibonacci.c
+++ Fibonacci.c
@@ -6,8 +6,7 @@ int main()
     scanf("%d",&n);
     for(int i=0;i<n;i++){
         int f=fib(i);
-        printf("%d Number of this series is %d\n",i,f);
-        
+        printf("%d Number of this series is %d\n",i,f);  
     }
     return 0;
 }
